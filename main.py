6ebcb970d89048b5d3e059a5c7d37832a1c55d0c
Midford0 <119308872+Midford0@users.noreply.github.com>
2023-02-08 20:02:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,80 @@
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
-    while low <= high:
-        mid = (low + high) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return low
+numbers = input('Введите любое количество чисел через пробел: ').split()
 
+#Преобразование введённой последовательности в список
+for i in range(len(numbers)):
+    numbers[i] = int(numbers[i])
 
-def insert_position(arr, target):
-    index = binary_search(arr, target)
-    if index < len(arr) and arr[index] == target:
-        return index
-    elif index == 0:
-        return 0
-    elif index == len(arr):
-        return len(arr)
+#Сортировка списка по возрастанию элементов в нем
+import operator
+
+def merge_sort(L, compare=operator.lt):
+    if len(L) < 2:
+        return L[:]
     else:
-        return index if target - arr[index - 1] <= arr[index] - target else index - 1
+        middle = int(len(L) / 2)
+        left = merge_sort(L[:middle], compare)
+        right = merge_sort(L[middle:], compare)
+        return merge(left, right, compare)
+def merge(left, right, compare):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
+list_of_numbers = merge_sort(numbers)
 
+print(f'''Список чисел, отсортированный по возрастанию:''', *(list_of_numbers))
 
-numbers = list(map(int, input("Введите последовательность чисел, разделенных пробелами: ").split()))
-numbers.sort()
-target = int(input("Введите число, чтобы найти его позицию: "))
-result = insert_position(numbers, target)
-print(f"Позиция для {target} это {result}")
+additional_number = int(input('Введите еще одно число: '))
+
+#Устанавливается номер позиции элемента, который меньше введенного пользователем числа, а следующий за ним больше или равен этому числу.
+#Ищем индекс определённого элемента в массиве через двоичный поиск
+def binary_search(array, element, left, right):
+    try:
+        if left > right:
+            return False
+
+        middle = (right + left) // 2
+        if array[middle] == element:
+            return middle
+        elif element < array[middle]:
+            return binary_search(array, element, left, middle - 1)
+        else:
+            return binary_search(array, element, middle + 1, right)
+    except IndexError:
+        return "Ошибка! Выходит за границы списка. Введите другое число." 
+#если есть числа, которые могут не соответствовать заданному условию
+
+#Устанавливается номер позиции элемента
+if not binary_search(list_of_numbers, additional_number, 0, len(list_of_numbers)):
+    n = min(list_of_numbers, key=lambda x: (abs(x - additional_number), x))
+    index = list_of_numbers.index(n)
+    minimum_index = index - 1
+    maximum_index = index + 1
+    if n < additional_number:
+        print(f'''Вашего элемента не было в списке.
+Индекс позиции ближайшего элемента, который МЕНЬШЕ введенного Вами числа: {index} (число  {n} ).
+Индекс позиции ближайшего элемента, который БОЛЬШЕ введенного Вами числа: {maximum_index} (число {list_of_numbers[maximum_index]} ).''')
+    elif n > additional_number:
+        print(f'''Вашего элемента не было в списке.
+Индекс позиции ближайшего элемента, который БОЛЬШЕ введенного Вами числа: {list_of_numbers.index(n)} (число {n} ).
+В списке нет элементов МЕНЬШЕ Вашего.''')
+    elif list_of_numbers.index(n) == 0:
+        print(f'''Номер позиции Вашего элемента в списке: {list_of_numbers.index(n)}''')
+    elif minimum_index < 0:
+        print(f'''Вашего элемента не было списке.
+Индекс позиции ближайшего элемента, который БОЛЬШЕ введенного Вами числа: {list_of_numbers.index(n)} (число {n} ).
+В списке нет элементов МЕНЬШЕ Вашего.''')
+else:
+    print(f'''Индекс позиции Вашего элемента в списке: {binary_search(list_of_numbers, additional_number, 0, len(list_of_numbers))}''')
